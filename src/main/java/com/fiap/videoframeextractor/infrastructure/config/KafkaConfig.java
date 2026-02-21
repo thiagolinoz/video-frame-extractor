3d4197d49b8473309022b94524049b7b3814c957
maycon-alves <maycon_valdemiro@hotmail.com>
2026-02-21 14:29:12 -0300

    update ip kafka

--- src/main/java/com/fiap/videoframeextractor/infrastructure/config/KafkaConfig.java
+++ src/main/java/com/fiap/videoframeextractor/infrastructure/config/KafkaConfig.java
@@ -22,7 +22,7 @@ import java.util.Map;
 @ConditionalOnProperty(name = "kafka.enabled", havingValue = "true")
 public class KafkaConfig {
 
-    @Value("${kafka.bootstrap-servers:100.53.138.200:9094}")
+    @Value("${kafka.bootstrap-servers:98.88.247.182:9094}")
     private String bootstrapServers;
 
     @Value("${kafka.consumer.group-id:video-frame-extractor-group-teste}")
