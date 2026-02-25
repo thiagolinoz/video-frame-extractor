bb837bca6b5c4dbfea4a7a1b2e9f068ccddf2787
maycon-alves <maycon_valdemiro@hotmail.com>
2026-02-24 21:51:06 -0300

    adicionando client s3

--- src/main/java/com/fiap/videoframeextractor/infrastructure/config/AwsS3Config.java
+++ src/main/java/com/fiap/videoframeextractor/infrastructure/config/AwsS3Config.java
@@ -11,6 +11,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3ClientBuilder;
+import software.amazon.awssdk.services.s3.S3CrtAsyncClientBuilder;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+
 
 @Configuration
 @Slf4j
@@ -25,26 +33,55 @@ public class AwsS3Config {
 
     @Value("${app.aws.s3.path-style-access:false}")
     private boolean pathStyleAccessEnabled;
+    @Bean
+    public S3AsyncClient s3AsyncClient() {
 
-    @Bean public AmazonS3 amazonS3Client() {
-        //log.info("Initializing AWS S3 client for region: {}", region);
+        S3CrtAsyncClientBuilder builder = S3AsyncClient.crtBuilder()
+                .region(Region.of(region))
+                .credentialsProvider(credentialsProvider());
 
-        AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard()
-                .withPathStyleAccessEnabled(pathStyleAccessEnabled);
-
-        if (s3Endpoint != null && !s3Endpoint.trim().isEmpty()) {
-            //log.info("Using custom S3 endpoint: {}", s3Endpoint);
-            builder.withEndpointConfiguration(
-                    new AwsClientBuilder.EndpointConfiguration(s3Endpoint, region)
-            );
-        } else {
-            builder.withRegion(region);
-        }
-
-        AmazonS3 s3Client = builder.build();
-
-        //log.info("AWS S3 client initialized successfully");
-        return s3Client;
+        return builder.build();
     }
+
+    @Bean
+    public S3TransferManager s3TransferManager(S3AsyncClient s3AsyncClient) {
+        return S3TransferManager.builder()
+                .s3Client(s3AsyncClient)
+                .build();
+    }
+
+    @Bean
+    public S3Client s3Client() {
+
+        S3ClientBuilder builder = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(credentialsProvider());
+
+        return builder.build();
+    }
+
+    private DefaultCredentialsProvider credentialsProvider() {
+        return  DefaultCredentialsProvider.builder().build();
+    }
+//    @Bean public AmazonS3 amazonS3Client() {
+//        //log.info("Initializing AWS S3 client for region: {}", region);
+//
+//        AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard()
+//                .withPathStyleAccessEnabled(pathStyleAccessEnabled);
+//
+//        if (s3Endpoint != null && !s3Endpoint.trim().isEmpty()) {
+//            //log.info("Using custom S3 endpoint: {}", s3Endpoint);
+//            builder.withEndpointConfiguration(
+//                    new AwsClientBuilder.EndpointConfiguration(s3Endpoint, region)
+//            );
+//        } else {
+//            builder.withRegion(region);
+//        }
+//
+//        AmazonS3 s3Client = builder.build();
+//
+//        //log.info("AWS S3 client initialized successfully");
+//        return s3Client;
+//    }
 
 }
